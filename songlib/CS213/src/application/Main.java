93723edd7481ad4bc179dfc47a25bdb140129ce2
rk714 <32375460+rk714@users.noreply.github.com>
2017-10-03 19:32:21 -0400

    Add files via upload

--- songlib/CS213/src/application/Main.java
+++ songlib/CS213/src/application/Main.java
@@ -1,48 +1,60 @@
 package application;
 	
+import java.awt.ScrollPane;
 import java.io.*;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
-
-
+import javafx.scene.text.Font;
+import javafx.beans.value.ChangeListener;
+import javafx.collections.*;
+import javafx.geometry.HPos;
 public class Main extends Application {
 	Scene scene;
-	ListView<String> listview;
-	
+	ListView<Song> listview;
+	Label label = new Label();
+	ObservableList<Song> obs= FXCollections.observableArrayList();;
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		/*try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}*/
+		
 		new Library("Songs");
 		listview = new ListView<>();
+		
+		label.setLayoutX(10);
+		label.setLayoutY(100);
+		label.setFont(Font.font("Arial",12));
+		
 		for(int i=0;i<Library.lib.size();i++){
-			listview.getItems().add(Library.lib.get(i).song +"\t" +Library.lib.get(i).artist + "\n");
+			obs.add(Library.lib.get(i));
 		}
+		listview.setItems(obs);
+		
+		listview.getSelectionModel().selectedItemProperty().addListener((obs,oldval,newval)->details(primaryStage));
 		listview.getSelectionModel().select(0);
-		/*listview.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>()){
-			@override
-			public void change(Observable<? extends String>, String a, String b){
-				
-			}
-		}*/
+		
 		primaryStage.setTitle("Playlist");
+		GridPane gridpane = new GridPane();
+		//GridPane.setHalignment(label, HPos.CENTER);
+		gridpane.add(label, 0, 0);
+		//ScrollPane scroll = new ScrollPane();
+		
 		VBox layout = new VBox(10);
-		layout.getChildren().addAll(listview);
+		layout.getChildren().addAll(listview, gridpane);
 		scene = new Scene(layout,300,250);
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+	
+	private void details(Stage a){
+		label.setText(listview.getSelectionModel().getSelectedItem().detail());
+		
 	}
 	
 	public static void main(String[] args) {
