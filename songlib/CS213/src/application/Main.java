4e6e5243908bec3840cc5a44c4830dd25544c5f9
Jason Holley <jason.holley@rutgers.edu>
2017-10-04 18:21:07 -0400

    fixed

--- songlib/CS213/src/application/Main.java
+++ songlib/CS213/src/application/Main.java
@@ -253,6 +253,11 @@ public class Main extends Application {
 		artist.setText("Artist: "+listview.getSelectionModel().getSelectedItem().artist);
 		if(listview.getSelectionModel().getSelectedItem().album != null){
 			album.setText("Album: "+listview.getSelectionModel().getSelectedItem().album);
+			year.setLayoutY(55);
+		}
+		else{
+			 album.setText(null);
+			 year.setLayoutY(40);
 		}
 		if(listview.getSelectionModel().getSelectedItem().year != null){
 			year.setText("Year: "+listview.getSelectionModel().getSelectedItem().year);
